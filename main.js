de8d93d9b08101329c704a71c05e60ca2437d03e
Darklord009 <72228239+Darklord009@users.noreply.github.com>
2021-10-02 12:57:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 var x = document.querySelectorAll("");
+
+document.getElementById("demo").innerHTML = "Hello Dolly.";
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
