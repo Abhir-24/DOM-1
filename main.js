f9e3e1770f37ec2e6c7eec5b896641a6358a1316
Darklord009 <72228239+Darklord009@users.noreply.github.com>
2022-10-02 19:20:39 +0530

    added move function feature

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ const person = {
   age: 50,
   eyeColor: "blue"
 };
+
+const move = () => {
+  const currency_one = currencyEl_one.value;
+  const currency_two =curTwoo.value;
+  fetch("https://open.exchangerate-api.com/v6/latest")
+    .then(res => res.json())
+    .then(data => {
+      //  console.log(data);
+      const rate = data.rates[currency_two] / data.rates[currency_one];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+    });
+}
+
+move();
